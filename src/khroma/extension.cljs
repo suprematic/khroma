753fff5026ffebd12128271c9d2ecd84aba2981a
Igor Tyulkanov <tyulkanov.igor@gmail.com>
2014-05-13 20:54:25 +0600

    refactoring

--- src/khroma/extension.cljs
+++ src/khroma/extension.cljs
@@ -6,14 +6,9 @@
 		[cljs.core.async.macros :refer [go]])
 )
 
-;depricated
-(defn send-request [extension-id request]
-	(let [ch (async/chan)]
-		(.sendRequest js/chrome.extension extension-id request
-			(fn [response]
-				(go
-					(>! ch (js->clj response))
-					(async/close ch)))) ch))
+(defn send-request "Depricated function" [extension-id request]
+	(with-callback
+		#(.send-request js/chrome.extension extension-id request %)))
 
 (defn get-url [path]
 	(.getURL js/chrome.extension path))
@@ -27,21 +22,23 @@
 (defn get-extension-tabs [windowId]
 	(.getExtensionTabs js/chrome.extension))
 
-(defn is-allowed-incognito-access
-	(let [ch (async/chan)]
-		(.isAllowedIncognitoAccess js/chrome.extension
-			(fn [is-allowed-access]
-				(go
-					(>! ch (js->clj is-allowed-access))
-					(async/close ch)))) ch))
+(defn allowed-incognito-access?
+	(with-callback
+		#(.isAllowedIncognitoAccess js/chrome.extension %)))
 
-(defn is-allowed-file-scheme-access
-	(let [ch (async/chan)]
-		(.isAllowedFileSchemeAccess js/chrome.extension
-			(fn [is-allowed-access]
-				(go
-					(>! ch (js->clj is-allowed-access))
-					(async/close ch)))) ch))
+(defn allowed-file-scheme-access?
+	(with-callback
+		#(.isAllowedFileSchemeAccess js/chrome.extension %)))
 
 (def set-update-url-data [data]
 	(.setUpdateUrlData js/chrome.extension data))
+
+(defn- with-callback [f]
+	(let [ch (async/chan)]
+		(f (make-handler ch)) ch))
+
+(defn- make-handler [ch]
+	(fn [data]
+		(go
+			(>! ch data)
+			(async/close ch))))
