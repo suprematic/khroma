a1cce54b33196eb844efb7acee6e32e10555ce7a
Igor Tyulkanov <tyulkanov.igor@gmail.com>
2014-05-14 11:32:47 +0600

    clojure style guide

--- src/khroma/extension.cljs
+++ src/khroma/extension.cljs
@@ -1,44 +1,43 @@
 (ns khroma.extension
-	(:require
-		[cljs.core.async :as async])
+  (:require
+    [cljs.core.async :as async])
 
-	(:require-macros
-		[cljs.core.async.macros :refer [go]])
-)
+  (:require-macros
+    [cljs.core.async.macros :refer [go]]))
 
 (defn send-request "Depricated function" [extension-id request]
-	(with-callback
-		#(.send-request js/chrome.extension extension-id request %)))
+  (with-callback
+    #(.send-request js/chrome.extension extension-id request %)))
 
 (defn get-url [path]
-	(.getURL js/chrome.extension path))
+  (.getURL js/chrome.extension path))
 
 (defn get-views [fetchProperties]
-	(.getViews js/chrome.extension fetchProperties))
+  (.getViews js/chrome.extension fetchProperties))
 
 (defn get-background-page 
-	(.getBackgroundPage js/chrome.extension))
+  (.getBackgroundPage js/chrome.extension))
 
 (defn get-extension-tabs [windowId]
-	(.getExtensionTabs js/chrome.extension))
+  (.getExtensionTabs js/chrome.extension))
 
 (defn allowed-incognito-access?
-	(with-callback
-		#(.isAllowedIncognitoAccess js/chrome.extension %)))
+  (with-callback
+    #(.isAllowedIncognitoAccess js/chrome.extension %)))
 
 (defn allowed-file-scheme-access?
-	(with-callback
-		#(.isAllowedFileSchemeAccess js/chrome.extension %)))
+  (with-callback
+    #(.isAllowedFileSchemeAccess js/chrome.extension %)))
 
 (def set-update-url-data [data]
-	(.setUpdateUrlData js/chrome.extension data))
+  (.setUpdateUrlData js/chrome.extension data))
 
 (defn- with-callback [f]
-	(let [ch (async/chan)]
-		(f (make-handler ch)) ch))
+  (let [ch (async/chan)]
+    (f (make-handler ch)) ch))
 
 (defn- make-handler [ch]
-	(fn [data]
-		(go
-			(>! ch data)
-			(async/close ch))))
+  (fn [data]
+    (go
+      (>! ch data)
+      (async/close ch))))
