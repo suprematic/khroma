d6b5235254213ef0234d82edf2dc2f5b2794e13b
Alexey Aristov <aav@acm.org>
2014-06-30 18:53:27 +0200

    remove cljsbuild plugin

--- project.clj
+++ project.clj
@@ -10,6 +10,4 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
         [org.clojure/clojurescript "0.0-2234"]
         [org.clojure/core.async "0.1.267.0-0d7780-alpha"]]
-  
-  :plugins [[lein-cljsbuild "1.0.2"]]
 )
