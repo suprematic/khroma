140547de1c8a88470e46b21700dc87b069bd59c3
Gary111 <tyulkanov.igor@gmail.com>
2014-07-22 11:22:07 +0700

    tab listeners was added

--- src/khroma/tabs.cljs
+++ src/khroma/tabs.cljs
@@ -1,6 +1,7 @@
 (ns khroma.tabs
   (:require 
     [khroma.log :as log]
+    [clojure.walk :as walk]
     [cljs.core.async :as async])
   
   (:require-macros 
@@ -14,3 +15,21 @@
         (go 
           (>! ch (js->clj tab))
           (async/close ch)))) ch))
+
+(defn tab-updated-events []
+  (let [ch (async/chan)]
+    (.addListener js/chrome.tabs.onUpdated
+      (fn [id info tab]
+        (async/put! ch (walk/keywordize-keys (js->clj {:tabId id :changeInfo info :tab tab}))))) ch))
+
+(defn tab-removed-events []
+  (let [ch (async/chan)]
+    (.addListener js/chrome.tabs.onRemoved
+      (fn [id info]
+        (async/put! ch (walk/keywordize-keys (js->clj {:tabId id :removeInfo info}))))) ch))
+
+(defn tab-replaced-events []
+  (let [ch (async/chan)]
+    (.addListener js/chrome.tabs.onReplaced
+      (fn [added removed]
+        (async/put! ch (walk/keywordize-keys (js->clj {:added added :removed removed}))))) ch))
