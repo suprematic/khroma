11b9f751c39bc2df53e12fa74814977a2fdf4c89
Ricardo J. Mendez <ricardo@numergent.com>
2015-09-17 16:24:40 +0300

    Fixed typo

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
 
   :source-paths  ["src"]
 
+
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.122"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
