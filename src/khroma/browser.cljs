8ecf31ca0d38592fb668a551cd1bef2e964f49b4
Ricardo J. Mendez <ricardo@numergent.com>
2015-09-18 10:39:42 +0300

    New browser namespace, on-click function

---
+++ src/khroma/browser.cljs
@@ -0,0 +1,17 @@
+(ns khroma.browser
+  (:require
+    [cljs.core.async :refer [chan >!]])
+  (:require-macros
+    [cljs.core.async.macros :refer [go go-loop]]))
+
+
+(defn on-click
+  "Returns a channel where we'll put a tab when we get an onClicked event"
+  []
+  (let [ch (chan)]
+    (.addListener js/chrome.browserAction.onClicked
+                  (fn [tab]
+                    (go
+                      (>! ch tab))))
+    ch))
+
