e141f4b859dfad3a6e16093f7e6f85ab6e8b1cf3
Ricardo J. Mendez <ricardo@numergent.com>
2015-10-18 12:29:14 +0300

    Upgrading ClojureScript version

--- project.clj
+++ project.clj
@@ -9,6 +9,6 @@
 
 
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.122"]
+                 [org.clojure/clojurescript "1.7.145"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]]
    )
